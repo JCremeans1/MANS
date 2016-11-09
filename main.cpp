e193bf886b4bf5fc042040f9d748fad7e9777cdb
JCremeans1 <Jcremeans@nevada.unr.edu>
2016-11-08 21:37:39 -0800

    Update 1

--- main.cpp
+++ main.cpp
@@ -35,16 +35,16 @@ public:
 
 //Variables
 
-    int l = 10;
-    int valx;
-    int valy;
-    int evalx;
-    int evaly;
-    int evaltotal;
-    double evalxtotal = 0;
-    double evalytotal = 0;
-    double sumx = 0;
-    double sumy = 0;
+int l = 10;
+int valx;
+int valy;
+int evalx;
+int evaly;
+int evaltotal;
+double evalxtotal = 0;
+double evalytotal = 0;
+double sumx = 0;
+double sumy = 0;
 
 
 agent::agent()
@@ -93,29 +93,29 @@ void agent::evaluate()
     for (int i = 0; i <= k; ++i)
     {
 
-    evalx = abs(archivex.at(i)- archivex.at(k-i));
-    evalxtotal = evalxtotal + evalx;
+        evalx = abs(archivex.at(i)- archivex.at(k-i));
+        evalxtotal = evalxtotal + evalx;
 
-    evaly = abs(archivey.at(i) - archivey.at(k-i));
-    evalytotal = evalytotal + evaly;
+        evaly = abs(archivey.at(i) - archivey.at(k-i));
+        evalytotal = evalytotal + evaly;
 
         noveltyofx.push_back(evalxtotal);
         noveltyofy.push_back(evalytotal);
 
     }
 
-        sumx = accumulate(noveltyofx.begin(), noveltyofx.end(), 0.0);
-        sumy = accumulate(noveltyofy.begin(), noveltyofy.end(), 0.0);
-        novelty = sumx+sumy;
+    sumx = accumulate(noveltyofx.begin(), noveltyofx.end(), 0.0);
+    sumy = accumulate(noveltyofy.begin(), noveltyofy.end(), 0.0);
+    novelty = sumx+sumy;
 
-        noveltyvector.push_back(novelty);
+    noveltyvector.push_back(novelty);
 
 
-    cout << "Novelty Vector: " << endl;
-        for (vector<double>::const_iterator i = noveltyvector.begin(); i != noveltyvector.end(); ++i)
-        {
-            std::cout << *i << ' ';
-        }
+//    cout << "Novelty Vector: " << endl;
+//        for (vector<double>::const_iterator i = noveltyvector.begin(); i != noveltyvector.end(); ++i)
+//        {
+//            std::cout << *i << ' ';
+//        }
 
 
     cout << " " << endl;
@@ -131,26 +131,27 @@ int main()
     cout << " " << endl;
 
 
-vector<agent>agents(3);
-for (unsigned int m = 0; m< agents.size(); m++)
-{
-    agents[m].clean();
-   for (int i = 0; i <= 10; i++)
+    vector<agent>agents(3);
+    for (unsigned int m = 0; m< agents.size(); m++)
     {
-    agents[m].initialize();
-    agents[m].create();
-    agents[m].evaluate();
-    evalytotal = 0;
-    evalxtotal = 0;
-    sumx = 0;
-    sumy = 0;
-    cout << agents[0].novelty << " ?? " << agents[1].novelty << endl;
-    assert(agents[0].novelty != agents[1].novelty);
+        agents[m].clean();
+        for(int j = 0; j < agents.size(); j++)
+        {
+            agents[m].initialize();
+        }
+            agents[m].create();
+            agents[m].evaluate();
+            evalytotal = 0;
+            evalxtotal = 0;
+            sumx = 0;
+            sumy = 0;
+//    cout << agents[0].novelty << " ?? " << agents[1].novelty << endl;
+//    assert(agents[0].novelty != agents[1].novelty);
 
-    cout << " End of Run: " << i << endl;
-    cout << " " << endl;
+            cout << " " << endl;
+            cout << "End of Run: "<< endl;
+            cout << " " << endl;
     }
-}
     cout << " " << endl;
     cout << "Done"<< endl;
 
